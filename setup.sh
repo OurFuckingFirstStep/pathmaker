6f7e7642f29d43dd23a1d5af099e6bd56b8be82b
John Liekezer <cool.Bakov@yandex.ru>
2016-09-15 00:52:43 +0300

    Add setup.sh

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+file_not_cgi_bin="drawing.svg"
+
+if [[ $USER != "root" ]]; then
+    echo "You must be root. Use \"sudo $(basename \"$0\")\" "
+else
+    cp cgi-bin/* -r /usr/lib/cgi-bin/
+    if [[ $file_not_cgi_bin != "" ]]; then
+        cp $file_not_cgi_bin -r /var/www/html/
+    fi
+    chmod 766 /var/www/html/
+    chmod 766 /var/www/html/*
+fi
