0737a9b60ec74f3d8af7c15c7ce9dd21b784683b
Pavel <pavel.semenenko@phystech.edu>
2016-09-15 00:33:55 +0300

    Added exeption handling

--- cgi-bin/path.py
+++ cgi-bin/path.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from pathMap import PathMap
+from pathMap import PathMap, NoPath, UnknownVertex
 import cgi
 import cgitb
 
@@ -13,37 +13,57 @@ RESULT_SVG_PREFIX = r'drawing_path_'
 if DEBUG == 1:
     cgitb.enable()
 
+class BadInputFormat(Exception):
+    pass
 
-#headers, essentiaal!
-print (r'Content-type: text/html')
-print ('\r\n\r\n')
-
-#HTML-code
-print ("<html><head>")
-print ("<title>PathMaker (beta)</title>")
-#print ("<meta http-equiv=\"refresh\" content=\"2\">")
-print ("</head>")
-
-
-print ("<body>")
-
-form = cgi.FieldStorage()
-
-if 'from' in form and 'to' in form:
-    A, B = form['from'].value, form['to'].value
-else:
-    A, B = 3, 6
-print ('From:', A, '<br>')
-print ('To:', B, '<br>')
+def get_points():
+    form = cgi.FieldStorage()
     
-#    if A.isdigit() and B.isdigit():
-if True:
-    name = RESULT_SVG_PREFIX + str(A) + str(B) + '.svg'
-    print ('<img src=\"/', name, '\"><br>', sep='')
-    path = PathMap(PATH_TO_SVG + BASE_SVG_NAME)
-    path.create_path(A, B, PATH_TO_SVG + name)
-        
-#else:
-#    print ('Bad input<br>')
+    if 'from' not in form or 'to' not in form:
+        raise BadInputFormat
+#        A, B = 1, 2
+    else:
+        A, B = form['from'].value, form['to'].value
 
-print("</body></html>")
+    if not A.isdigit() or not B.isdigit():
+#    if False:
+        raise BadInputFormat
+
+    return A, B
+
+def print_headers():
+#headers, essentiaal!
+    print (r'Content-type: text/html')
+    print ('\r\n\r\n')
+
+def print_html():
+    #HTML-code
+    print ("<html><head>")
+    print ("<title>PathMaker (beta)</title>")
+    #print ("<meta http-equiv=\"refresh\" content=\"2\">")
+    print ("</head>")
+    print ("<body>")
+
+    try:
+        A, B = get_points()
+        print ('From:', A, '<br>')
+        print ('To:', B, '<br>')
+    
+        name = RESULT_SVG_PREFIX + str(A) + str(B) + '.svg'
+        path = PathMap(PATH_TO_SVG + BASE_SVG_NAME)
+        path.create_path(A, B, PATH_TO_SVG + name)
+
+        print ('<img src=\"/', name, '\"><br>', sep='')
+        print("</body></html>")
+
+    except BadInputFormat:
+        print (r'<p>Bad input format!</p>')
+    except (NoPath, UnknownVertex):
+        print (r"<p>Can't build path between points", A, 'and', B,"</p>")
+    
+def main():
+    print_headers()
+    print_html()
+
+if __name__ == "__main__":
+    main()
